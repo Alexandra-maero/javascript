a600160a041c6c26ae91b9b9e33e262105255487
Alexandra Maero <alexandramaero12@gmail.com>
2020-10-25 11:54:58 -0300

    variables and operators

---
+++ variables-y-operadores/index.js
@@ -0,0 +1,25 @@
+//Variables y Operadores
+/* Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.*/
+
+var alquilerMensual, expensasMensuales, gastosMensuales;
+alquilerMensual = 12000
+expensasMensuales = 4000
+
+gastosMensuales = alquilerMensual + expensasMensuales;
+console.log(gastosMensuales + ' pesos argentinos');
+
+/*Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.*/
+
+var nombre, lugar;
+nombre = "Sabina";
+lugar = "River";
+
+console.log(nombre + ' anunció que tocará en ' + lugar);
+
+/*Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado la suma una 3er variable (utilizar length).*/
+
+var estrofaUno, estrofaDos; 
+estrofaUno = "Este virus que no muere ni nos mata,";
+estrofaDos = "Esta amnesia en el cielo del paladar."
+
+console.log(estrofaUno.length + estrofaDos.length);
